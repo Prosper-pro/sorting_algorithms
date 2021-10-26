dc0245c16eb88e2a9c6251d3f261282a0f8b0737
knightiti <ktess2008@gmail.com>
2021-10-26 16:19:59 -0700

    <Updated header file>

--- sort.h
+++ sort.h
@@ -29,5 +29,10 @@ void quick_sort(int *array, size_t size);
 void qs(int *arr, int low, int high, size_t size);
 int partition(int *arr, int low, int high, size_t size);
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif
+#endif /*SORT_H*/
